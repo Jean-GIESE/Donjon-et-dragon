54f68d5c323b982c825839829e39d7f1cce69a38
Jean GIESE <jean.giese@etu.unistra.fr>
2025-05-07 09:06:16 +0200

    Ajout des attributs et de 2 méthodes dans Entite.java

--- src/entite/Entite.java
+++ src/entite/Entite.java
@@ -2,5 +2,20 @@ package entite;
 
 public abstract class Entite
 {
-
+    protected m_nom;
+    protected m_pv;
+    protected m_force;
+    protected m_dexterite;
+    protected m_vitesse;
+    protected m_initiative;
+    
+    public void attaquer();
+    {
+    
+    }
+    
+    public void seDeplacer()
+    {
+    
+    }
 }
