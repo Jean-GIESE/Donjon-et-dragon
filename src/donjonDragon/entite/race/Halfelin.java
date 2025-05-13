5c1a8b9f40c2f82f170092be8a2e77cc276d33c3
Hugo Leger <legerh@unistra.fr>
2025-05-13 09:32:42 +0200

    race : Ajout des caractéristiques nul

--- src/donjonDragon/entite/race/Halfelin.java
+++ src/donjonDragon/entite/race/Halfelin.java
@@ -4,7 +4,10 @@ public class Halfelin extends Race
 {
     public Halfelin()
     {
+        m_pv = 0;
+        m_force = 0;
         m_dexterite = 4;
         m_vitesse = 2;
+        m_initiative = 0;
     }
 }
