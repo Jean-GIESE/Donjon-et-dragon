c2b4c3119a475a38eebfebd03d7ad106e9f4fd85
Jean GIESE <jean.giese@etu.unistra.fr>
2025-05-13 09:44:40 +0200

    SRC:Modifications des constructeurs d'équipements

--- src/donjonDragon/equipement/Armure.java
+++ src/donjonDragon/equipement/Armure.java
@@ -5,8 +5,10 @@ public class Armure extends Equipement
     private String m_nom;
     private int m_classeArmure;
     
-    public Armure()
+    public Armure(String nom, int classeArmure, boolean est_lourd)
     {
-    
+        m_nom = nom;
+        m_classeArmure = classeArmure;
+        m_lourd = est_lourd;
     }
 }
