8b279f561a802096237795bea5c5513b289691d2
Hugo Leger <legerh@unistra.fr>
2025-05-13 10:08:45 +0200

    SRC : Ajout de la méthode lancer dans De

--- src/donjonDragon/De.java
+++ src/donjonDragon/De.java
@@ -1,9 +1,13 @@
 package donjonDragon;
 
+import java.util.Random;
+
 public class De
 {
     private int m_nombre;
     private int m_face;
+
+    Random rand = new Random();
     
     public De(int nombre, int face)
     {
@@ -11,8 +15,13 @@ public class De
         m_face = face;
     }
     
-    public void lancer()
+    public int lancer(int nombre, int face)
     {
-    
+        int total=0;
+        for(int i=0;i<nombre;i++)
+        {
+            total+= rand.nextInt(1,face);
+        }
+        return total;
     }
 }
