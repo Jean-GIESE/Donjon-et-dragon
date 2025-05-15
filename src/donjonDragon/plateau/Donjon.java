96fbda7366c50fbaab0a34c4166406b10583fb2c
Jean GIESE <jean.giese@etu.unistra.fr>
2025-05-15 15:58:23 +0200

    SRC:Amélioration du constructeur de Donjon

--- src/donjonDragon/plateau/Donjon.java
+++ src/donjonDragon/plateau/Donjon.java
@@ -10,7 +10,8 @@ public class Donjon
     public Donjon()
     {
         m_taille = this.creerCarte();
-        m_carte = new String[m_taille][m_taille];
+//         m_carte = new String[m_taille][m_taille];
+        m_carte = this.initialiserCarte();
     }
         
     public int creerCarte()
@@ -34,13 +35,28 @@ public class Donjon
         return nb;
     }
     
+    public String[][] initialiserCarte()
+    {
+        String[][] carte = new String[m_taille][m_taille];
+        for (int i=0; i<m_taille; i++)
+        {
+            for (int j=0; j<m_taille; j++)
+            {
+                carte[i][j] = ".";
+            }
+        }
+        return carte;
+    }
+    
     public void afficherCarte()
     {
 //         String[] alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 //         
+//         
+//         
 //         for (int i=1; i<=alhapbet; i++)
 //         {
-//             m_carte[0][i] = 
+//             
 //         }
     }
 }
