8264f09265939ddf36835aa58da6080e7be2c9cc
Hugo Leger <legerh@unistra.fr>
2025-05-21 10:02:28 +0200

    SRC : Guerison fini, a test

--- src/donjonDragon/entite/sort/Guerison.java
+++ src/donjonDragon/entite/sort/Guerison.java
@@ -1,19 +1,31 @@
 package donjonDragon.entite.sort;
 
+import donjonDragon.De;
 import donjonDragon.entite.Entite;
-import donjonDragon.entite.Personnage;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Guerison implements Sort{
     private Scanner m_scanner;
+
+    public Guerison() {
+        m_scanner = new Scanner(System.in);
+    }
+
     @Override
     public void lancer(ArrayList<Entite> entites)
     {
         listPerso(entites);
         Entite cible = entites.get(Integer.parseInt(m_scanner.nextLine())-1);
-
+        De UnDeDix = new De(1,10);
+        int resSoin = UnDeDix.lancer();
+        if(cible.getPv()+resSoin>cible.getPvMax()){
+            cible.setPv(cible.getPvMax());
+        }
+        else{
+            cible.setPv(cible.getPv()+resSoin);
+        }
     }
     public void listPerso(ArrayList<Entite> persos)
     {
