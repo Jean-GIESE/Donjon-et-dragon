a2806a689618482237dbb4e6383971f0025a3914
Hugo Leger <legerh@unistra.fr>
2025-05-26 16:40:10 +0200

    SRC : sort overhaul

--- src/donjonDragon/entite/sort/ArmeMagique.java
+++ src/donjonDragon/entite/sort/ArmeMagique.java
@@ -1,12 +1,13 @@
 package donjonDragon.entite.sort;
 
 import donjonDragon.entite.Entite;
+import donjonDragon.plateau.Donjon;
 
 import java.util.ArrayList;
 
 public class ArmeMagique implements Sort{
     @Override
-    public void lancer(ArrayList<Entite> entites) {
+    public void lancer(ArrayList<Entite> entites, Donjon donjon) {
 
     }
 }
