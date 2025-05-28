0ba7483dca627268e833ba7e9ae3be03e5f4e121
Jean GIESE <jean.giese@etu.unistra.fr>
2025-05-28 12:10:03 +0200

    SRC:Modifications des races, changement des attributs en privée

--- src/donjonDragon/entite/classe/Clerc.java
+++ src/donjonDragon/entite/classe/Clerc.java
@@ -1,9 +1,24 @@
 package donjonDragon.entite.classe;
 
+import donjonDragon.equipement.*;
+
+import java.util.ArrayList;
+
 public class Clerc extends Classe
 {
     public Clerc()
     {
-        m_pv = 16;
+        super("Clerc", 16, this.initialiserEquipement());
+    }
+    
+    @Override
+    public ArrayList<Equipement> initialiserEquipement()
+    {
+        ArrayList<Equipement> equipementsParDefaut = new ArrayList<>();
+        equipementsParDefaut.add(new Arme("Masse d'armes",  new De(1, 6), 1, false));
+        equipementsParDefaut.add(new Armure("Armure d'écailles", 9, false));
+        equipementsParDefaut.add(new Arme("Arbalète légère", new De(1, 8), 16, false));
+        
+        return equipementsParDefaut;
     }
 }
--- src/donjonDragon/entite/classe/Roublard.java
+++ src/donjonDragon/entite/classe/Roublard.java
@@ -1,9 +1,24 @@
 package donjonDragon.entite.classe;
 
+import donjonDragon.equipement.*;
+
+import java.util.ArrayList;
+
 public class Roublard extends Classe
 {
     public Roublard()
     {
+        super("Roublard", 16, this.initialiserEquipement());
         m_pv = 16;
+    }
+    
+    @Override
+    public ArrayList<Equipement> initialiserEquipement()
+    {
+        ArrayList<Equipement> equipementsParDefaut = new ArrayList<>();
+        equipementsParDefaut.add(new Arme("Rapière", new De(1, 8), 1, true));
+        equipementsParDefaut.add(new Arme("Arc court",  new De(1, 6), 16, false));
+        
+        return equipementsParDefaut;
     }
 }
